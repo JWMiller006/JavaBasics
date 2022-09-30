db5356b45cbc43d64e4ad8782864c948a5da4d33
J Miller <95261008+JWMiller006@users.noreply.github.com>
2022-09-30 10:39:14 -0400

    main

--- My Libraries/Arrays/Mathw/Arrays.java
+++ My Libraries/Arrays/Mathw/Arrays.java
@@ -1,31 +1,32 @@
-
-// Totals the array
-public static int arrTot(int[] arrTemp, int arrLen)
+class Arrays
 {
     // Totals the array
-    // Sets the total to 0 to add to it
-    int output = 0;
-    // Repeats until all the numbers have been added
-    for(int i = 0; i < arrLen; i++)
+    public static int arrTot(int[] arrTemp, int arrLen)
     {
-        output += arrTemp[i];
+        // Totals the array
+        // Sets the total to 0 to add to it
+        int output = 0;
+        // Repeats until all the numbers have been added
+        for(int i = 0; i < arrLen; i++)
+        {
+            output += arrTemp[i];
+        }
+        // Returns the Sum
+        return output;
     }
-    // Returns the Sum
-    return output;
-}
 
-// Returns the Product of an array
-public static int arrProd(int[] arrTemp, int arrLen)
-{
-    // Totals the array
-    // Sets the total to 1 to multiply  it
-    int output = 1;
-    // Repeats until all the numbers have been added
-    for(int i = 0; i < arrLen; i++)
+    // Returns the Product of an array
+    public static int arrProd(int[] arrTemp, int arrLen)
     {
-        output *= arrTemp[i];
+        // Totals the array
+        // Sets the total to 1 to multiply  it
+        int output = 1;
+        // Repeats until all the numbers have been added
+        for(int i = 0; i < arrLen; i++)
+        {
+            output *= arrTemp[i];
+        }
+        // Returns the Product
+        return output;
     }
-    // Returns the Product
-    return output;
 }
-
